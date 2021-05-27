debc7ad6a7fb8b772935012d92ea3b930987f0c5
Mihail Cartera <vip.sfsfd@gmail.com>
2021-05-27 16:45:25 +0300

    Spend-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import SpendItem from "./components/SpendItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <SpendItem></SpendItem>
     </div>
   );
 }
---
+++ src/components/SpendItem.js
@@ -0,0 +1,14 @@
+import "./SpendItem.css";
+
+function SpendItem() {
+  return (
+    <div className="spend-item">
+      <div>28 Martie, 2021</div>
+      <div className="spend-item__description">
+        <h2>Asigurare Medicala</h2>
+        <div className="spend-item__price">350 RON</div>
+      </div>
+    </div>
+  );
+}
+export default SpendItem;
