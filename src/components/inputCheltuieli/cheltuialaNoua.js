f19e86471b8fe67e3a94ac1ebc4b6a53a4792d2e
Mihail(admin) <mcartera47@gmail.com>
2021-06-24 15:41:16 +0300

    input item, using state, communication child-parent

--- src/components/inputCheltuieli/cheltuialaNoua.js
+++ src/components/inputCheltuieli/cheltuialaNoua.js
@@ -5,9 +5,18 @@ import Form from "./form";
 
 
 const cheltuialaNoua = () =>{
+const saveInputItemHandler = (inputItem) => {
+    const item = {
+        ...inputItem,
+        id: Math.random().toString(),
+    };
+
+    console.log(item);
+};
+
 return(
 <div className="new-expense">
-    <Form />
+    <Form saveInputItem={saveInputItemHandler}/>
 </div>
 );
 };
--- src/components/inputCheltuieli/form.js
+++ src/components/inputCheltuieli/form.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./form.css";
 
-const form = () => {
-    return(
-        <form>
-                <div className="new-expense__controls">
-                    <div className="new-expense__control">
-                        <label>Titlu</label>
-                        <input type="text" />
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Pret</label>
-                        <input type="number" min="0.01" step="0.01"/>
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Data</label>
-                        <input type="date" min="2019-01-01" max="2022-12-31" />
-                    </div>
+const Form = (props) => {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const titleChangeHandler = event => {
+        setTitle(event.target.value);
+    }
+    const amountChangeHandler = event => {
+        setAmount(event.target.value);
+    }
+    const dateChangeHandler = event => {
+        setDate(event.target.value);
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const item = {
+            title: title,
+            amount: amount,
+            date: new Date(date)
+        }
+        props.saveInputItem(item);
+        setTitle('');
+        setAmount('');
+        setDate('');
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Titlu</label>
+                    <input type="text"
+                        value={title}
+                        onChange={titleChangeHandler} />
                 </div>
-                <div className="new-expense__actions">
-                    <button type="submit">Adauga</button>
+                <div className="new-expense__control">
+                    <label>Pret</label>
+                    <input type="number" min="0.01" step="0.01"
+                        value={amount}
+                        onChange={amountChangeHandler} />
                 </div>
+                <div className="new-expense__control">
+                    <label>Data</label>
+                    <input type="date" min="2019-01-01" max="2022-12-31"
+                        value={date}
+                        onChange={dateChangeHandler} />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Adauga</button>
+            </div>
         </form>
     );
 };
-export default form;
+export default Form;
