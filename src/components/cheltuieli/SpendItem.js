ef7452361c4403cbff4a5b72e3586176eeb443e2
Mihail(admin) <mcartera47@gmail.com>
2021-06-24 12:59:56 +0300

    mini use state(example)

--- src/components/cheltuieli/SpendItem.js
+++ src/components/cheltuieli/SpendItem.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./SpendItem.css";
 import SpendDate from "./SpendDate";
 import Card from "../generalUI/Card";
 
 const SpendItem = (props) => {
+  const [title,setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  }
   return (
     <Card className="spend-item">
         <SpendDate date={props.date}/>
       <div className="spend-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="spend-item__price">{props.pret} RON</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>  Change Title</button>
     </Card>
   );
 }
